9c383512082f04ce00822a70e10e1bb52eef927c
= <guy.harper@gmail.com>
2017-10-29 14:30:00 +0000

    Setting up voteShare calculation

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,14 +7,52 @@ import SeatsDisplay from './SeatsDisplay';
 class Main extends React.Component {
   state = {
     constituencies: [],
-    totalSeats: []
+    voteShare: [
+      { lab: 0 },
+      { con: 0 },
+      { ld: 0 },
+      { snp: 0 },
+      { pc: 0 },
+      { green: 0 },
+      { ukip: 0 },
+      { other: 0 },
+      { dup: 0 },
+      { sf: 0 }
+    ],
+    totalSeats: [
+      { lab: 0 },
+      { con: 0 },
+      { ld: 0 },
+      { snp: 0 },
+      { pc: 0 },
+      { green: 0 },
+      { ukip: 0 },
+      { other: 0 },
+      { dup: 0 },
+      { sf: 0 }
+    ]
   }
 
   componentWillMount() {
     Axios
       .get('/api/constituencies')
-      .then(res => this.setState({ constituencies: res.data }))
+      .then(res => this.setState({ constituencies: res.data }, () => this.getVoteShare()))
       .catch(err => console.log(err));
+  }
+
+  getVoteShare() {
+    const partyCodes = this.state.voteShare.map((e) => Object.keys(e)[0]);
+    const partyCodes2017 = partyCodes.map((e) => e.concat('2017'));
+    const partyResults = [];
+    partyCodes2017.forEach((party) => {
+      const partyResult = this.state.constituencies.reduce((sum, constituency) => {
+        return sum + constituency[party];
+      }, 0);
+      partyResults.push({
+        [party]: partyResult
+      });
+    });
+    this.setState({ voteShare: partyResults });
   }
 
   render() {
@@ -26,7 +64,7 @@ class Main extends React.Component {
           </div>
           <div>
             <h1>2017 General Election</h1>
-            <VoteShareChart constituencyData={this.state.constituencies} />
+            <VoteShareChart constituencyData={this.state.voteShare} />
             <SeatsDisplay constituencyData={this.state.constituencies} />
           </div>
         </section>
