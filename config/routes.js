e00cd47a5d46e414e4f764eaaf343063c9bbff1e
= <guy.harper@gmail.com>
2017-10-31 22:35:23 +0000

    Setting up authentication on backend

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
 const constituencies  = require('../controllers/constituencies');
+const auth  = require('../controllers/auth');
+const secureRoute = require('../lib/secureRoute');
 
 router.route('/constituencies')
-  .get(constituencies.index);
-  
+  .get(secureRoute, constituencies.index);
+
+router.route('/register')
+  .post(auth.register);
+
+router.route('/login')
+  .post(auth.login);
+
 router.all('/*', (req, res) => res.notFound());
 
 module.exports = router;
