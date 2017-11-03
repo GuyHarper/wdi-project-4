f8521c2f33f7f5c723c4636cc9aae89bd5dd0d5d
= <guy.harper@gmail.com>
2017-11-03 08:59:34 +0000

    Styling

--- src/components/utility/Navbar.js
+++ src/components/utility/Navbar.js
@@ -12,16 +12,16 @@ const Navbar = ({ history }) => {
   }
 
   return(
-    <nav className="navbar">
-      <div className="navbar-left">
+    <nav className="navbar navbar-expand-lg">
+      <div className="navbar-nav mr-auto">
         <div className="navbar-brand">
           <Link to="/">Election Mapper</Link>
         </div>
       </div>
-      <div className="navbar-right">
-        {!Auth.isAuthenticated() && <Link to="/login">Login</Link>}
-        {!Auth.isAuthenticated() && <Link to="/register">Register</Link>}
-        {Auth.isAuthenticated() && <a href="#" onClick={logout}>Logout</a>}
+      <div className="navbar-nav ml-auto">
+        {!Auth.isAuthenticated() && <Link to="/login" className="nav-item nav-link">Login</Link>}
+        {!Auth.isAuthenticated() && <Link to="/register" className="nav-item nav-link">Register</Link>}
+        {Auth.isAuthenticated() && <a href="#" onClick={logout} className="nav-item nav-link">Logout</a>}
       </div>
     </nav>
   );
