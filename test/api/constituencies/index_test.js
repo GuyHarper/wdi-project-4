b4ccce764a237af2ced29627e14cec028b8ff1f7
= <guy.harper@gmail.com>
2018-01-06 21:02:59 +0000

    Adding more server side tests

--- test/api/constituencies/index_test.js
+++ test/api/constituencies/index_test.js
@@ -22,4 +22,55 @@ describe('GET /api/constituencies', () => {
       .set('Accept', 'application/json')
       .expect(200, done);
   });
+  it('should return an array', done => {
+    api
+      .get('/api/constituencies')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        expect(res.body).to.be.an('array');
+        done();
+      });
+  });
+  it('should return the correct number of elements', done => {
+    api
+      .get('/api/constituencies')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        expect(res.body.length).to.equal(constituencyData.length);
+        done();
+      });
+  });
+  it('should return the correct data', done => {
+    api
+      .get('/api/constituencies')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        constituencyData.forEach((constituencyDataElement, index) => {
+          const constituency = res.body[index];
+          expect(constituency.id).to.be.a('string');
+          expect(constituency.name).to.equal(constituencyDataElement.name);
+          expect(constituency.code).to.equal(constituencyDataElement.code);
+          expect(constituency.country).to.equal(constituencyDataElement.country);
+          expect(constituency.region).to.equal(constituencyDataElement.region);
+          expect(constituency.electorate2017).to.equal(constituencyDataElement.electorate2017);
+          expect(constituency.totalValidVotes2017).to.equal(constituencyDataElement.totalValidVotes2017);
+          expect(constituency.totalInvalidVotes2017).to.equal(constituencyDataElement.totalInvalidVotes2017);
+          expect(constituency.con2017).to.equal(constituencyDataElement.con2017);
+          expect(constituency.lab2017).to.equal(constituencyDataElement.lab2017);
+          expect(constituency.ld2017).to.equal(constituencyDataElement.ld2017);
+          expect(constituency.ukip2017).to.equal(constituencyDataElement.ukip2017);
+          expect(constituency.green2017).to.equal(constituencyDataElement.green2017);
+          expect(constituency.snp2017).to.equal(constituencyDataElement.snp2017);
+          expect(constituency.pc2017).to.equal(constituencyDataElement.pc2017);
+          expect(constituency.dup2017).to.equal(constituencyDataElement.dup2017);
+          expect(constituency.sf2017).to.equal(constituencyDataElement.sf2017);
+          expect(constituency.sdlp2017).to.equal(constituencyDataElement.sdlp2017);
+          expect(constituency.uup2017).to.equal(constituencyDataElement.uup2017);
+          expect(constituency.alliance2017).to.equal(constituencyDataElement.alliance2017);
+          expect(constituency.other2017).to.equal(constituencyDataElement.other2017);
+          expect(constituency.otherWinner2017).to.equal(constituencyDataElement.otherWinner2017);
+        });
+        done();
+      });
+  });
 });
